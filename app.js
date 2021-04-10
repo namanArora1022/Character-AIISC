da748d87051e35c82f4f7e2fb0e037cda1e39ef2
Naman Arora <71423632+namanArora1022@users.noreply.github.com>
2021-04-10 11:26:19 +0530

    deployed

--- app.js
+++
@@ -1,7 +0,0 @@
-const key = document.getElementById('key');
-const code = document.getElementById('code');
-
-document.addEventListener('keypress' , (e)=>{
-    key.innerHTML = e.key;
-    code.innerHTML = e.keyCode;
-});
